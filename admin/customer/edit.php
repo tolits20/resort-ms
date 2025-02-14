eae2b6064cb4949a7f2483fa9c2a062efd0c7d62
S1ngularty <asherxd10245@gmail.com>
2025-02-14 20:09:30 +0800

    customer index

--- admin/customer/edit.php
+++ admin/customer/edit.php
@@ -1,6 +1,7 @@
 <?php 
 
 include('../includes/template.html');
+include('../../resources/database/config.php');
 
 ?> 
 <style>
--- admin/customer/index.php
+++ admin/customer/index.php
@@ -1,19 +1,45 @@
 <?php 
 include ('../includes/template.html');
+include('../../resources/database/config.php');
 
-$sql1="SELECT * FROM customer";
-mysqli_query($conn,$sql1);
+
+$sql1="SELECT * FROM account";
+$result=mysqli_query($conn,$sql1);
+
+if(mysqli_affected_rows($conn)>0){
+
 
 ?>
+<style>
+    table th,td{
+    vertical-align: middle;
+    width: auto;
+    padding: 10px;
+    text-align: justify;
+    }
+</style>
 <div class="content">
-    <table>
+   <div class="div1"></div>
+   <table  class='table table-striped' >
         <tr>
-            <th>username</th>
+            <th>Username</th>
             <th>Role</th>
             <th>Action</th>
         </tr>
-        <tr>
-            <?php ?>
-        </tr>
+        
+            <?php 
+            while($row=mysqli_fetch_assoc($result)){
+                print "<tr>
+                <td>{$row['username']}</td>
+                <td>{$row['role']}</td>
+                <td><a href='view.php?id={$row['account_id']}' class='btn btn-primary btn-sm'><i class='fas fa-eye'></i></a>
+                <a href='edit.php?id={$row['account_id']}' class='btn btn-primary btn-sm'><i class='fas fa-edit'></i></a>
+                <a href='delete.php?id={$row['account_id']}' class='btn btn-danger btn-sm'><i class='fas fa-trash'></i></a></td>
+                </tr>"
+                ;
+            }
+    }
+            ?>
+        
     </table>
 </div>
