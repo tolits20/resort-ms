ad0335f4faf20baf9c7aab44e7ba90bddd93f377
S1ngularty <asherxd10245@gmail.com>
2025-02-16 00:58:15 +0800

    updated Database

--- resources/database/resort_ms.sql
+++ resources/database/resort_ms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 13, 2025 at 04:29 PM
+-- Generation Time: Feb 15, 2025 at 05:57 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -38,15 +38,15 @@ CREATE TABLE IF NOT EXISTS `account` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`account_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `account`
 --
 
 INSERT INTO `account` (`account_id`, `username`, `password`, `role`, `status`, `created_at`, `updated_at`) VALUES
-(13, 'levipenaverde@example.com', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'admin', 'activate', '2025-02-13 11:36:07', '2025-02-13 11:36:07'),
-(14, 'tolits@example.com', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'user', 'activate', '2025-02-13 13:56:31', '2025-02-13 13:56:31');
+(5, 'levipenaverde@example.com', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'admin', 'activate', '2025-02-14 15:10:14', '2025-02-14 15:10:14'),
+(9, 'tolits@example.com', 'cbfdac6008f9cab4083784cbd1874f76618d2a97', 'user', 'activate', '2025-02-15 05:24:00', '2025-02-15 08:13:20');
 
 -- --------------------------------------------------------
 
@@ -89,12 +89,43 @@ CREATE TABLE IF NOT EXISTS `payments` (
 
 CREATE TABLE IF NOT EXISTS `room` (
   `room_id` int(11) NOT NULL AUTO_INCREMENT,
-  `room_ number` int(11) NOT NULL,
+  `room_code` varchar(50) NOT NULL,
   `type` enum('standard','premium') NOT NULL,
   `status` enum('available','booked','under maintenance') NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`room_id`)
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`room_id`),
+  UNIQUE KEY `room_ code` (`room_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`room_id`, `room_code`, `type`, `status`, `created_at`, `updated_at`) VALUES
+(9, 'room101', 'premium', 'available', '2025-02-15 15:37:50', '2025-02-15 16:22:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room_gallery`
+--
+
+CREATE TABLE IF NOT EXISTS `room_gallery` (
+  `room_id` int(11) NOT NULL,
+  `room_img` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  KEY `galley_room_fk` (`room_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `room_gallery`
+--
+
+INSERT INTO `room_gallery` (`room_id`, `room_img`, `created_at`) VALUES
+(9, '167b0b4ce07b71.png', '2025-02-15 15:37:50'),
+(9, '167b0bf3c6b0be.png', '2025-02-15 16:22:20'),
+(9, '167b0bf3c6b747.png', '2025-02-15 16:22:20');
 
 -- --------------------------------------------------------
 
@@ -105,7 +136,8 @@ CREATE TABLE IF NOT EXISTS `room` (
 CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `account_id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
   `age` int(11) NOT NULL,
   `gender` enum('male','female') NOT NULL,
   `contact` varchar(15) NOT NULL,
@@ -113,18 +145,19 @@ CREATE TABLE IF NOT EXISTS `user` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`user_id`),
-  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `name` (`fname`),
   UNIQUE KEY `contact` (`contact`),
-  KEY `user_account_fk` (`account_id`)
+  KEY `user_account_fk` (`account_id`),
+  KEY `unique_fullname` (`fname`,`lname`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `account_id`, `name`, `age`, `gender`, `contact`, `profile_img`, `created_at`, `updated_at`) VALUES
-(8, 13, 'Levi Asher   Penaverde', 20, 'male', '987867546', '67add9278008a0.53893895.jpg', '2025-02-13 11:36:07', '2025-02-13 11:36:07'),
-(9, 14, 'Tolits Jacalan', 21, 'female', '9876544466', '67adfa0fead136.77894954.jpg', '2025-02-13 13:56:31', '2025-02-13 13:56:31');
+INSERT INTO `user` (`user_id`, `account_id`, `fname`, `lname`, `age`, `gender`, `contact`, `profile_img`, `created_at`, `updated_at`) VALUES
+(5, 5, 'Levi Asher', 'Penaverde', 20, 'male', '987867546', '67af5cd6192634.90705227.jpg', '2025-02-14 15:10:14', '2025-02-14 15:10:14'),
+(9, 9, 'Angelito', 'Jacalan', 20, 'male', '9876544461', '67b04ca01eed56.63896355.jpg', '2025-02-15 05:24:00', '2025-02-15 08:13:20');
 
 --
 -- Constraints for dumped tables
@@ -142,6 +175,12 @@ ALTER TABLE `booking`
 --
 ALTER TABLE `payments`
   ADD CONSTRAINT `payment_book_fk` FOREIGN KEY (`book_id`) REFERENCES `booking` (`book_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `room_gallery`
+--
+ALTER TABLE `room_gallery`
+  ADD CONSTRAINT `galley_room_fk` FOREIGN KEY (`room_id`) REFERENCES `room` (`room_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `user`
